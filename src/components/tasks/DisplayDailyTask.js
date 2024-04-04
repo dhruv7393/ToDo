38c40ae0d78b4639c4c05ad604cc59bfadb84ef9
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-04-04 11:09:35 -0400

    Update dailytask pending logic

--- src/components/tasks/DisplayDailyTask.js
+++ src/components/tasks/DisplayDailyTask.js
@@ -32,7 +32,7 @@ export default function DisplayDailyTask(props) {
   React.useEffect(()=>{
     axios
       .get(process.env.REACT_APP_BACKEND_URL + "dailytask/updatecount")
-      .then((data) => console.log("Daily task updated"))
+      .then((data) => {})
       .then(()=> loadTasks())
       .catch((err) => console.log(err));
   },[])
@@ -52,7 +52,7 @@ export default function DisplayDailyTask(props) {
       <Stack spacing={{ xs: 1 }} direction="row" useFlexGap flexWrap="wrap">
         {(taskList.length &&
           taskList.map((task) => {
-            let { _id, title, done, edited, pending, surplus } = task;
+            let { _id, title, done, edited, pending } = task;
               return (
                 <Card
                   key={"Daily" + _id}
@@ -73,30 +73,23 @@ export default function DisplayDailyTask(props) {
                             markAsChecked({
                               _id,
                               title,
-                              done: pending == 1 ? true : false,
+                              done: pending >= 0 ? true : false,
                               edited,
-                              pending: pending > 0 ? pending -1 :0,
-                              surplus: pending > 0 ? surplus : surplus+1,
+                              pending: pending +1
                             })
                           }
                         >
-                          {pending > 0  && <RemoveCircleOutlineIcon
-                            color={"" + (!parseInt(pending) && "primary")}
-                          /> || <AddCircleOutlineIcon
+                          <AddCircleOutlineIcon
                             color={""}
-                          />}
+                          />
                         </IconButton>
                     }
                     avatar={
-                      <>
-                      {pending && <Avatar sx={avatarColor(5)} aria-label="recipe">
-                        {pending}
-                      </Avatar> || <></>}
-                      {(surplus || pending==0) && <Avatar sx={avatarColor(1)} aria-label="recipe">
-                        {surplus}
-                      </Avatar> || <></>}</>
+                      <Avatar sx={pending <0 ?avatarColor(5):avatarColor(1)} aria-label="recipe">
+                        {Math.abs(pending)}
+                      </Avatar>
                     }
-                    title={title==="Goon"? <LightTooltip title={textToBeDisplayed(pending, surplus)}> <Button>{title}</Button></LightTooltip> : title}
+                    title={title==="Goon"? <LightTooltip title={textToBeDisplayed(pending)}> <Button>{title}</Button></LightTooltip> : title}
                   />
                 </Card>
               );
@@ -106,14 +99,14 @@ export default function DisplayDailyTask(props) {
   );
 }
 
-const textToBeDisplayed = (pending, surplus) =>{
+const textToBeDisplayed = (pending) =>{
   const goono = ["Sahaj uncle","Kishanji","Chirag bhai","Aashish bhai","Jigar bhai","Tejal bhabhi","Vinay bhai","Riddhi ben","Jamshedji tata - dilip kumar","Jayant uncle","Lalit uncle","Yash bhai","Miral bhai","Vivek bhai","Hetal ben","Prashant bhai","Rupal bhabhi","Viru bhai","Malav bhai","Hardik bhai","Dharti ben","Yogin bhai","Manmeet bhai","Mayank bhai","Ankur bhai","Rahul bhai","Meet bhai","Satish bhai","Harsh bhai","Kesin bhai","Aashish bhai","Shail bhai","Raghav bhai","Rishabh bhai","Rujool bhai","Deep bhai","Ujval bhai","Advait bhai","Ruchit bhai kadakiya","Amit bhai","Jaymin bhai","Vrund bhai","Jaydeep bhai","Jaymin bhai pickup","Jay bhai la","Mayur bhai","Nipun bhai","Grinish bhai","Sujeet bhai","Rahul bhai sf/canada","Gokul bhai","Jignesh bhai","Manan bhai","Spandan bhai","Daddy","Alpesh bhai","Shasvat bhai","Anmol bhai","Sneha bhabhi","Dhruvalbhai","Nisarg bhai","Aashish bhai Pandya","Sarvam bhai dr","Punyesh bhai","Yash bhai Boston","Deep bhai reno","Bhargav bhai reno","Darshan bhai reno","Vivek bhai fresno","Ronak bhai","Dr.saheb","Jimit bhai","Divyang bhai","Priyank bhai","Jal bhai","Anil bhai","Milan bhai nj","Jay bhai paji","Dhrumil bhai vora","Kirit bhai","Divyesh bhai","Sashank bhai","Purvadarshan bhai","Ambrish bhai","Sukumar bhai","Sarvam swami bus 500 bus","Jugal bhai","Gaurav bhai","Manthan bhai","Krutarth bhai","Kunj bhai","Milind bhai","Harbir Bhai Protek","Sambhav bhai Protek","Dhruvbhai Patel texas","Maa","Papa","Satish papa","Mona mummy","Priyank jiju","Disha didi","Mauli","Payal didi","Ram","Shubham","Bhavana","Dominique","Arpit","Vivek","Mini"]
 
   let valueToBeCalculated = ((Math.ceil(
     (new Date(new Date().toLocaleDateString()) - new Date('3/4/2024')) /
       (1000 * 60 * 60 * 24)
-  )) -(pending -1) + surplus)%22
-  console.log(valueToBeCalculated)
+  )) +pending)%22
+  
   let value = (valueToBeCalculated%(goono.length/5)) < 1? 1 : (valueToBeCalculated%(goono.length/5))
   let notes = (goono.slice((value-1)*5, (value*5)))
   return (
