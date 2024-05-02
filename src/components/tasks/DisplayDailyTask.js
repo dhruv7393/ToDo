1d4ac6a844c66ebcdbc098b8ae41aa2aa963bf70
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-05-02 09:24:46 -0400

    Fix for goon

--- src/components/tasks/DisplayDailyTask.js
+++ src/components/tasks/DisplayDailyTask.js
@@ -89,7 +89,7 @@ export default function DisplayDailyTask(props) {
                         {Math.abs(pending)}
                       </Avatar>
                     }
-                    title={title==="Goon"? <LightTooltip title={textToBeDisplayed(pending)}> <Button>{title}</Button></LightTooltip> : title}
+                    title={title==="Goon"? <LightTooltip title={textToBeDisplayed(pending, edited)}> <Button>{title}</Button></LightTooltip> : title}
                   />
                 </Card>
               );
@@ -99,16 +99,17 @@ export default function DisplayDailyTask(props) {
   );
 }
 
-const textToBeDisplayed = (pending) =>{
+const textToBeDisplayed = (pending, edited) =>{
   const goono = ["Sahaj uncle","Kishanji","Chirag bhai","Aashish bhai","Jigar bhai","Tejal bhabhi","Vinay bhai","Riddhi ben","Jamshedji tata - dilip kumar","Jayant uncle","Lalit uncle","Yash bhai","Miral bhai","Vivek bhai","Hetal ben","Prashant bhai","Rupal bhabhi","Viru bhai","Malav bhai","Hardik bhai","Dharti ben","Yogin bhai","Manmeet bhai","Mayank bhai","Ankur bhai","Rahul bhai","Meet bhai","Satish bhai","Harsh bhai","Kesin bhai","Aashish bhai","Shail bhai","Raghav bhai","Rishabh bhai","Rujool bhai","Deep bhai","Ujval bhai","Advait bhai","Ruchit bhai kadakiya","Amit bhai","Jaymin bhai","Vrund bhai","Jaydeep bhai","Jaymin bhai pickup","Jay bhai la","Mayur bhai","Nipun bhai","Grinish bhai","Sujeet bhai","Rahul bhai sf/canada","Gokul bhai","Jignesh bhai","Manan bhai","Spandan bhai","Daddy","Alpesh bhai","Shasvat bhai","Anmol bhai","Sneha bhabhi","Dhruvalbhai","Nisarg bhai","Aashish bhai Pandya","Sarvam bhai dr","Punyesh bhai","Yash bhai Boston","Deep bhai reno","Bhargav bhai reno","Darshan bhai reno","Vivek bhai fresno","Ronak bhai","Dr.saheb","Jimit bhai","Divyang bhai","Priyank bhai","Jal bhai","Anil bhai","Milan bhai nj","Jay bhai paji","Dhrumil bhai vora","Kirit bhai","Divyesh bhai","Sashank bhai","Purvadarshan bhai","Ambrish bhai","Sukumar bhai","Sarvam swami bus 500 bus","Jugal bhai","Gaurav bhai","Manthan bhai","Krutarth bhai","Kunj bhai","Milind bhai","Harbir Bhai Protek","Sambhav bhai Protek","Dhruvbhai Patel texas","Maa","Papa","Satish papa","Mona mummy","Priyank jiju","Disha didi","Mauli","Payal didi","Ram","Shubham","Bhavana","Dominique","Arpit","Vivek","Mini"]
 
-  let valueToBeCalculated = ((Math.ceil(
-    (new Date(new Date().toLocaleDateString()) - new Date('4/30/2024')) /
+  let valueToBeCalculated = Math.ceil(
+    (new Date(edited) - new Date('5/1/2024') + ((pending+7)* 1000 * 60 * 60 * 24)) /
       (1000 * 60 * 60 * 24)
-  )) +pending)%22
-  
-  let value = (valueToBeCalculated%(goono.length/5)) < 1? 1 : (valueToBeCalculated%(goono.length/5))
-  let notes = (goono.slice((value-1)*5, (value*5)))
+  )%22
+
+  let value = (valueToBeCalculated%(goono.length/5)) < 0? 0 : (valueToBeCalculated%(goono.length/5))
+  let notes = (goono.slice((value)*5, (value*5)+5))
+
   return (
       <>
           {
